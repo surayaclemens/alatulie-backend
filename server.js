5141f8d9c866266992b7f68ae83cbcdc3e982def
Samantha Stockall <sam.stockall@hotmail.com>
2022-06-09 19:18:19 -0400

    Main logic built out, working on .gets for different routes

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+//Maybe include public/static at later time 
+
+const translations = require('/routes/translations');
+app.use('/translations', translations);
+
+app.listen(8080, () => {
+    console.log('Server is listening');
+})
